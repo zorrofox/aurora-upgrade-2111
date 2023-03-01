fe74d34bb74e06ee7d075822d013f40b0d757881
Greg Huang <debin.huang@gmail.com>
2023-03-01 09:04:03 +0800

    Initial commit

---
+++ test/aurora-upgrade-2.11.1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AuroraUpgrade2111 from '../lib/aurora-upgrade-2.11.1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aurora-upgrade-2.11.1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AuroraUpgrade2111.AuroraUpgrade2111Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
